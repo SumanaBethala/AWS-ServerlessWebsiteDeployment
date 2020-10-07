61debe8dd68b7f840d876ae71c35a4fb4f33eb72
SumanaBethala <51401976+SumanaBethala@users.noreply.github.com>
2020-10-07 19:19:29 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+window._config = {
+    cognito: {
+        userPoolId: '[Your Pool ID]', // e.g. us-east-1_pmFIfHzS8
+        userPoolClientId: '[Your Pool AppClient ID]', // e.g. 5vrp7sf0n0j21i94u5sb2297jo
+        region: '[Your Region]' // e.g. us-east-1
+    },
+    api: {
+        invokeUrl: '[Your API Invoke URL]' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
+
